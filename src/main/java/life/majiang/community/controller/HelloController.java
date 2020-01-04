3067b5a3a14cdcb11be39618f14412702c588ac2
chenzhaobo <chenzhaobo126@qq.com>
2020-01-05 00:49:17 +0800

    Initial commit

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description
+ * @auther 1
+ * @create 2020-01-04 17:34
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}

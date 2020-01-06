d3e14b918dccd2a04f6b6c23a9b5cf2b71f73ed5
chenzhaobo <chenzhaobo126@qq.com>
2020-01-06 17:35:53 +0800

    完成github授权，新增发布页面

---
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -0,0 +1,40 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description
+ * @auther 1
+ * @create 2020-01-04 17:34
+ */
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        return "index";
+    }
+}
--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -3,16 +3,10 @@ package life.majiang.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * @Description
- * @auther 1
- * @create 2020-01-04 17:34
- */
 @Controller
-public class HelloController {
-
-    @GetMapping("/")
-    public String index(){
-        return "index";
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
     }
 }
